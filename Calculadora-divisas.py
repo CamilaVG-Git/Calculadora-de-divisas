2ede55c5543e498c72b1392493e399d846194404
Camila <camilavg2309@gmail.com>
2026-03-09 17:02:39 -0400

    Errores arreglados

--- Calculadora-divisas.py
+++ Calculadora-divisas.py
@@ -1,29 +1,23 @@
-def exchange_money(amount, from_currency, to_currency):
-    rates = {
-        'USD': 1.0,
-        'JPY': 0.0075,
-        'EUR': 1.09,
-        'MXN': 0.059,
-        'DOP': 0.017
-    }
+def exchange_money(budget, exchange_rate):
+    return budget / exchange_rate
 
-    if from_currency not in rates or to_currency not in rates:
-        print("Error: moneda no válida.")
-        return None
-    
-    usd = amount * rates[from_currency]
 
-    result = usd / rates[to_currency]
- 
-    return result
+rates = {
+    "JPY": 0.0075,
+    "EUR": 1.09,
+    "MXN": 0.059,
+    "DOP": 0.017
+}
 
 print("Calculadora de divisas")
-print("Monedas disponibles: USD, JPY, EUR, MXN")
-amount = float(input("Ingresa la cantidad de dinero: "))
-from_currency = input("Moneda que tienes: ").upper()
-to_currency = input("Moneda a la que quieres convertir: ").upper()
+print("Monedas disponibles: JPY, EUR, MXN, DOP")
 
-converted = exchange_money(amount, from_currency, to_currency)
+budget = float(input("Ingresa la cantidad en USD: "))
+currency = input("Moneda a la que quieres convertir: ").upper()
 
-if converted is not None:
-    print("Resultado:", converted, to_currency)
+if currency in rates:
+    exchange_rate = rates[currency]
+    result = exchange_money(budget, exchange_rate)
+    print("Resultado:", result, currency)
+else:
+    print("Error: moneda no válida")
